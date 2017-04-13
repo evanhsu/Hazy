f5703c4dd3de8945aef937b3037fa1b0865f4369
Christopher Baron <topherbaron@gmail.com>
2017-04-13 18:23:22 -0400

    added fb

--- templates/page.js
+++ templates/page.js
@@ -18078,6 +18078,14 @@ module.exports = p => `
                 <div>hazyshade&#64;gmail.com</div>
                 <div>Mon - Sat 11am - 8pm</div>
                 <div>Mon - Sat 12pm - 7pm</div>
+                <a href="https://www.facebook.com/Hazy-Shade-Disc-Golf-And-More-173084619405424" target="_blank">
+                    <svg class="facebook" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="90px" height="90px" viewBox="0 0 90 90" xml:space="preserve"><g>
+                        <path d="M90,15.001C90,7.119,82.884,0,75,0H15C7.116,0,0,7.119,0,15.001v59.998
+                            C0,82.881,7.116,90,15.001,90H45V56H34V41h11v-5.844C45,25.077,52.568,16,61.875,16H74v15H61.875C60.548,31,59,32.611,59,35.024V41
+                            h15v15H59v34h16c7.884,0,15-7.119,15-15.001V15.001z"/>
+                    </g></svg>
+                </a>
             </div>
         </div>
     </body>
