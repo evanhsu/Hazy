8db1ce64fb919094a779f4cd29b38cf0b6cdf641
Christopher Baron <topherbaron@gmail.com>
2017-04-14 19:16:34 -0400

    http(s)

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 require('node-env-file')( __dirname + '/.env' )
 
 const Fs = require('fs'),
-    router = require('./router')
+    router = require('./router'),
+    httpPort = process.env.HTTP_PORT || 80,
+    httpsPort = process.env.HTTPS_PORT || 443
 
-require('http').createServer( router ).listen( process.env.HTTP_PORT || 80 )
+require('http').createServer( ( request, response ) => {
+    response.writeHead( 301, { 'Location': `https://${process.env.DOMAIN}:${httpsPort}${request.url}` } )
+    response.end("")
+} ).listen( httpPort )
+
+console.log( `HTTP server listening at ${httpPort}` )
 
 require('https').createServer(
     { key: Fs.readFileSync( process.env.SSLKEY ), cert: Fs.readFileSync( process.env.SSLCERT ) },
     require('./router')
-).listen( process.env.HTTPS_PORT || 443 )
+).listen( httpsPort )
 
-/*
-require('./dal/Postgres').initialize()
-.then( () => {
-    console.log('DAL cached')
-
-    require('./wsApp').constructor (
-    require('http').createServer( require('./router') ).listen( process.env.PORT || 80 )
-    )
-
-    console.log( `An http server is running at port ${process.env.PORT || 80}` )
-    return Promise.resolve()
-} )
-.catch( e => console.log( `Error starting server: ${e.stack || e} ` ) )
-*/
+console.log( `HTTPS server listening at ${httpsPort}` )
