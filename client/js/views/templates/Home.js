d88de6a90c6d65b8c0b81dfc9e2046fd17b88a0a
Christopher Baron <topherbaron@gmail.com>
2017-04-27 14:58:24 -0400

    saving work

--- client/js/views/templates/Home.js
+++ client/js/views/templates/Home.js
@@ -1,21 +1,21 @@
 module.exports = p => 
 `<div>
     <img src="/static/img/behind-basket.jpg"/>
-    <div class="intro">
+    <div class="intro side-by-side">
         ${require('./lib/logoWhite')()}
         <div>
             <h3>Taking Disc Golf to a Whole New Level</h3>
-            <p>At Hazy Shade, disc golf is more than just a game...it's an obsession.  That's why since 2002, we've supplied the Miami Valley with the latest in disc golf gear and technology.</p>A
+            <p>At Hazy Shade, disc golf is more than just a game...it's an obsession.  That's why since 2002, we've supplied the Miami Valley with the latest in disc golf gear and technology.</p>
             <p>We also sponsor a number of tournaments and outings thoughout the year.  Become a Hazy Shader today to get the scoop on all the latest disc golf action!</p>
         </div>        
     </div>
-    <div class="featured-products">
-        <div>
-            <span>Feature Item #1</span>
+    <div class="side-by-side">
+        <div class="featured-product">
+            <span>Featured Item #1</span>
             <img src="/static/img/basket-close.jpg" />
         </div>
-        <div>
-            <span>Feature Item #2</span>
+        <div class="featured-product">
+            <span>Featured Item #2</span>
             <img src="/static/img/basket-close.jpg" />
         </div>
     </div>
@@ -26,7 +26,20 @@ module.exports = p =>
         </div>
         <div class="subtitle">New to the sport?  Find the perfect disc with out interactive guide</div>
     </div>
-    <div>
+    <div class="side-by-side">
+        <div class="ez-finder">
+            <span>E-Z Finder</span>
+            <svg>
+                <circle cx="115" cy="115" r="110"></circle>
+                <path d="M115,115 L115,5 A110,110 1 0,1 190,35 z"></path>
+                <text>Hazy E-Z Finder</text>
+                <text>An enlightened search experience awaits you</text>
+            </svg>
+        </div>
+        <div class="featured-product">
+            <span>Featured Item #3</span>
+            <img src="/static/img/basket-close.jpg" />
+        </div>
     </div>
     <div class="featured-event">
         <div>Join us for the 10th Annual</div>
