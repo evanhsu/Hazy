e3e5e746e822357dbbb4135c1321b2f68341008f
Christopher Baron <topherbaron@gmail.com>
2017-06-27 14:13:29 -0400

    updating byop players

--- client/js/views/Division.js
+++ client/js/views/Division.js
@@ -1,9 +1,19 @@
 module.exports = Object.assign( {}, require('./__proto__'), {
 
+    Byop: require('../models/Byop'),
+
     addTeam( team ) {
         this.model.data.players.push( team )
 
-        this.slurpTemplate( { template: `<li>${team.name1}, ${team.name2}</li>`, insertion: { el: this.els.players } } )
+        this.slurpTemplate( {
+            template: `<li><div><div><span>${team.name1}, ${team.name2}</span><span class="disc-selection">${this.getDiscLabel(team.disc1)}(${team.weight1}g), ${this.getDiscLabel(team.disc2)}(${team.weight2}g)</span></div></div></li>`,
+            insertion: { el: this.els.players }
+        } )
+    },
+
+    getDiscLabel( value ) {
+        const disc = this.discs.store.value[ value ]
+        return disc ? disc.label : 'Undecided'
     },
 
     notifyIfEmpty() {
@@ -12,6 +22,12 @@ module.exports = Object.assign( {}, require('./__proto__'), {
 
     postRender() {
         this.model.data.players = [ ]
+       
+        this.discs =
+            this.Model.constructor(
+                this.Byop.meta.discs.map( disc => ( { value: disc.value, label: disc.label.slice( 0, disc.label.indexOf('(') ) } ) ),
+                { storeBy: [ 'value' ] }
+            )
 
         return this
     }
