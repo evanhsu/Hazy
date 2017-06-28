71a0d7e8f6b51b27ec239165f3786cea6cb1f40e
Christopher Baron <topherbaron@gmail.com>
2017-06-27 20:41:41 -0400

    saving work

---
+++ client/js/views/Byop2016.js
@@ -0,0 +1,43 @@
+module.exports = Object.assign( {}, require('./__proto__'), {
+
+    Divisions: Object.create( require('../models/__proto__'), { resource: { value: 'division' } } ),
+    Byops: Object.create( require('../models/__proto__'), { resource: { value: 'byop' } } ),
+    WaitingList: Object.create( require('../models/__proto__'), { resource: { value: 'waiting-list' } } ),
+
+    events: {
+        byopRegisterLink: 'click'
+    },
+
+    onByopRegisterLinkClick() {
+        this.emit( 'navigate', '/byop' )
+    },
+
+    postRender() {
+
+        Promise.all( [ this.Divisions.get(), this.Byops.get() ] )
+        .then( () => this.renderPlayers() )
+        .catch( e => this.Error(e) )
+
+        this.WaitingList.get()
+        .then( () => this.renderWaitingList() )
+        .catch( e => this.Error(e) )
+
+        return this
+    },
+
+    renderPlayers() {
+
+        this.divisions = this.Divisions.data.reduce(
+            ( memo, division ) => Object.assign( memo, { [ division.id ]: this.factory.create( 'division', { model: { value: { data: division } }, insertion: { value: { el: this.els.divisions } } } ) } ),
+            { }
+        )
+
+        this.Byops.data.forEach( byop => this.divisions[ byop.divisionId ].addTeam( byop ) )
+
+        Object.keys( this.divisions ).forEach( divisionId => this.divisions[ divisionId ].notifyIfEmpty() )
+    },
+
+    renderWaitingList() {
+        this.WaitingList.data.forEach( datum => this.slurpTemplate( { template: `<li>${datum.name1}, ${datum.name2}</li>`, insertion: { el: this.els.waitingList } } ) )
+    }
+} )
---
+++ sql/schema/20170627.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "byop2015Results" (
+    "teamId"        VARCHAR(20),
+    "name1"        VARCHAR(100),
+    "player1Id"    VARCHAR(20),
+    "name2"        VARCHAR(100),
+    "player2Id"    VARCHAR(20),
+    "division"     VARCHAR(50),
+    "round1"        VARCHAR(20),
+    "round2"        VARCHAR(20),
+    "total"        VARCHAR(20),
+    "cash"          VARCHAR(20),
+    "1"          VARCHAR(50),
+    "2"          VARCHAR(50),
+    "3"          VARCHAR(50),
+    "4"          VARCHAR(50),
+    "5"          VARCHAR(50)
+);
